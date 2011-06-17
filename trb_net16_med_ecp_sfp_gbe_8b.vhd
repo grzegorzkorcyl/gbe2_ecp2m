a1630ca9121b64b55ec701875f7ad144e097937c
Grzegorz Korcyl <grzegorz.korcyl@gmail.com>
2011-06-17 20:03:18 +0200

    updated counters, xmit signal

--- trb_net16_med_ecp_sfp_gbe_8b.vhd
+++ trb_net16_med_ecp_sfp_gbe_8b.vhd
@@ -247,6 +247,8 @@ signal user_rst				: std_logic;
 signal reset_bsm			: std_logic_vector(3 downto 0);
 signal reset_debug			: std_logic_vector(31 downto 0);
 
+signal pcs_xmit                         : std_logic;
+
 begin
 
 -- Reset state machine for SerDes
@@ -294,7 +296,7 @@ clk_int : if (USE_125MHZ_EXTCLK = 0) generate
 		ff_tx_k_cntrl_ch0        => sd_tx_kcntl,
 		ff_rx_k_cntrl_ch0        => sd_rx_kcntl,
 			ff_rxfullclk_ch0         => sd_rx_clk,
-		ff_xmit_ch0              => '0',
+		ff_xmit_ch0              => pcs_xmit, --'0',
 		ff_correct_disp_ch0      => sd_tx_correct_disp,
 		ff_disp_err_ch0          => sd_rx_disp_error,
 		ff_cv_ch0                => sd_rx_cv_error,
@@ -342,7 +344,7 @@ clk_ext : if (USE_125MHZ_EXTCLK = 1) generate
 			ff_txhalfclk				=> open,
 			ff_txdata_ch0				=> sd_tx_data, -- TX data input
 			ff_tx_k_cntrl_ch0			=> sd_tx_kcntl, -- TX komma input
-			ff_xmit_ch0				=> '0', -- for autonegotiation (input)
+			ff_xmit_ch0				=> pcs_xmit, --'0', -- for autonegotiation (input)
 			ff_correct_disp_ch0		=> sd_tx_correct_disp, -- controls disparity at IPG start (input)
 			-- Resets and power down
 			ffc_quad_rst				=> quad_rst, -- async reset for whole QUAD (active high)
@@ -440,6 +442,8 @@ port map(
 	mr_an_enable			=> MR_AN_ENABLE_IN,
 	mr_restart_an			=> MR_RESTART_AN_IN,
 	mr_adv_ability			=> MR_ADV_ABILITY_IN,
+	
+	xmit_autoneg			=> pcs_xmit,
 	
 	force_isolate			=> '0',
 	force_loopback			=> '0',
